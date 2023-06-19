0f4c6de9dc229b19747cf3dbe708c4b8538590ef
vittoria <vittoriagarcia@gmail.com>
2023-06-18 22:58:02 -0400

    update

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/name_list"
+    //"URI": "mongodb://localhost/name_list"
+    "URI": "mongodb+srv://vittoriagarcia:JiNhlXQiMSbfbPuo@name-list.wqyuf1d.mongodb.net/name_list?retryWrites=true&w=majority"
 }
